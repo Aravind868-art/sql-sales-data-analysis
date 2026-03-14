fc05995da55649ca880c5db5472b32d132d359a8
Aravind868-art <boggulaaravind5@gmail.com>
2026-03-14 14:36:01 +0530

    Update employee_queries.sql

--- analysis/employee_queries.sql
+++ analysis/employee_queries.sql
@@ -1,11 +1,27 @@
--------------------------------------------------
---BASIC DATA VIEW
--------------------------------------------------
+-----------------------------
+--BASIC DATA UNDERSTANDING 
+-----------------------------
 
--- VIEW ALL EMPLOYEES
+-- Display all employees
 SELECT * 
 FROM EMP;
 
---VIEW ALL DEPARTMENTS
+--Display all departments
 SELECT *
 FROM DEPT;
+
+--Show employee names with their job roles
+SELECT ENAME,JOB
+FROM EMP;
+
+--Show employee names and their salaries
+SELECT ENAME,SAL
+FROM EMP;
+
+--List employees ordered by salary (highest first)
+SELECT ENAME,SAL
+FROM EMP
+ORDER BY SAL DESC;
+
+
+
